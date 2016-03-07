de190effbdc628eea41a14d1b9c932b0733f1866
James Sangalli <j.l.sangalli@gmail.com>
2016-03-07 15:24:19 +1300

    add files

---
+++ Default.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 23px;
+        }
+        .auto-style2 {
+            height: 30px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table style="width:100%;">
+            <tr>
+                <td>Double spend a transaction. </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">Enter your private key (do not use this address again after disclosing the private key) </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="privateKeyTextBox" runat="server" Width="644px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Enter the funding transaction output (select one output that has enough bitcoin to double spend your payment)</td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="txTextBox" runat="server" Width="642px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Enter the destination address that is to be double spent</td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="destinationTextBox" runat="server" Width="644px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Enter change address (double spend and other change will be sent to this address):</td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:TextBox ID="changeTextBox" runat="server" Width="644px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Enter the amount you wish to double spend (in whole bitcoin, e.g. 0.01 bitcoin)</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">
+                    <asp:TextBox ID="amountTextBox" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style1">
+                    Enter the amount you wish to be returned to your change address (remainder of tx output)</td>
+            </tr>
+            <tr>
+                <td class="auto-style1">
+                    <asp:TextBox ID="remainderTextBox" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Button ID="sendButton" runat="server" OnClick="sendButton_Click" Text="Send" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using NBitcoin;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading;
+using NBitcoin.Protocol;
+
+public partial class _Default : System.Web.UI.Page
+{
+    /*
+    1)This program is for experimentation purposes only. 
+    2)It is not to be used to maliciously double spend other bitcoin users.
+    3)This program may or may not be successful in performing a double spend 
+    4)All responsibility is on the user
+    5)You must run a node to be able to broadcast the transaction.
+    */
+    public void doubleSpend (string address, string change, Decimal fee)
+    {
+        var blockr = new BlockrTransactionRepository();
+        Transaction fundingTransaction = blockr.Get(txTextBox.Text);
+
+        Transaction payment = new Transaction();
+        payment.Inputs.Add(new TxIn()
+        {
+            PrevOut = new OutPoint(fundingTransaction.GetHash(), 1)
+        });
+
+        BitcoinSecret s = new BitcoinSecret(privateKeyTextBox.Text, Network.Main);
+
+        var destination = BitcoinAddress.Create(address);
+        var changeAddress = BitcoinAddress.Create(change);
+
+        payment.Outputs.Add(new TxOut()
+        {
+            Value = Money.Coins(Decimal.Parse(amountTextBox.Text)),
+            ScriptPubKey = destination.ScriptPubKey //goes to destination address
+        });
+        payment.Outputs.Add(new TxOut()
+        {
+            Value = Money.Coins(Decimal.Parse(remainderTextBox.Text) - fee),
+            //fee is subtracted from change amount and goes to the miners as the difference
+            ScriptPubKey = changeAddress.ScriptPubKey //goes back as change
+        });
+
+        var message = "Double Spend!";
+        var bytes = Encoding.UTF8.GetBytes(message);
+        payment.Outputs.Add(new TxOut()
+        {
+            Value = Money.Zero,
+            ScriptPubKey = TxNullDataTemplate.Instance.GenerateScriptPubKey(bytes)
+        });
+        Console.WriteLine(payment);
+
+        payment.Inputs[0].ScriptSig = fundingTransaction.Outputs[1].ScriptPubKey;
+        payment.Sign(s, false);
+
+        
+        using (var node = Node.ConnectToLocal(Network.Main)) //Connect to the node
+        {
+            node.VersionHandshake(); //Say hello
+            //Advertize your transaction (send just the hash)
+            node.SendMessage(new InvPayload(InventoryType.MSG_TX, payment.GetHash()));
+            //Send it
+            node.SendMessage(new TxPayload(payment));
+            Thread.Sleep(500); //Wait a bit
+        }
+
+    }
+
+    protected void sendButton_Click(object sender, EventArgs e)
+    {
+        if (txTextBox.Text != "" || destinationTextBox.Text != "" || privateKeyTextBox.Text != "" || remainderTextBox.Text != "" || amountTextBox.Text != "" || changeTextBox.Text != "")
+        {
+            doubleSpend(destinationTextBox.Text,changeTextBox.Text, 0m); //pays no fee so it is low priority
+            doubleSpend(changeTextBox.Text, changeTextBox.Text, 0.001m); //pays higher fee, gets higher priority and causes double spend
+        }
+        else
+        {
+            MessageBox.Show("Please try again", "Invalid or incomplete info provided", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+    }
+}
