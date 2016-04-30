98d63aab765e0a4925be7f3a3db02b90b13ea7da
James Sangalli <j.l.sangalli@gmail.com>
2016-04-30 15:53:30 +1200

    Update Default.aspx.cs

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -7,13 +7,6 @@ using NBitcoin.Protocol;
 
 public partial class _Default : System.Web.UI.Page
 {
-    /*
-    1)This program is for experimentation purposes only. 
-    2)It is not to be used to maliciously double spend other bitcoin users.
-    3)This program may or may not be successful in performing a double spend 
-    4)All responsibility is on the user
-    5)You must run a node to be able to broadcast the transaction.
-    */
     public void doubleSpend (string address, string change, Decimal fee)
     {
         var blockr = new BlockrTransactionRepository();
